d37045e3b11c3c960d5c015641288e49e11d3fb1
norm-fun-code <normanc41@gmail.com>
2026-09-09 18:20:32 -0400

    Add offline shell caching

---
+++ world-of-wisdom-3d/sw.js
@@ -0,0 +1,1 @@
+const C='wow3d-v2';const A=['./','./index.html','./styles.css','./app-1.js','./app-2.js','./app-3.js','./data-regions.js','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
